d28b05364d03b2d22e1dfde55fb7d259eae094fb
privera <privera@celerity.com>
2020-06-30 21:02:57 -0400

    write as json

--- data_structures/daily_diary.py
+++ data_structures/daily_diary.py
@@ -1,25 +1,63 @@
+# When using the diary app
+# the user can save entries to file
+# or read the entries by displaying them in the console
+
+
+import json
 from datetime import datetime
 
 
 class Diary:
 
-   
+    FILENAME = 'diary.json'
+    entries = []
+
+    def load(self):
+        try:
+            with open(self.FILENAME, 'r') as f:
+                self.entries = json.loads(f.read())
+        except FileNotFoundError:
+            return []
+
+    def save(self):
+        with open(self.FILENAME, 'w') as f:
+            f.write(json.dumps(self.entries))
+    
+    def add_entry(self, entry):
+        self.entries.append(entry.serialize())
+
+    def show_all_entries(self):
+        for entry in self.entries:
+            description = entry.get('description')
+            created = entry.get('created')
+            formatted_entry = f'The entry created on {created} is: {description} \n'
+            print(formatted_entry)
 
 
-
-
-class DiaryEntry:
+class Entry:
     def __init__(self):
         self.description = None
         self.created = datetime.now().strftime("%c")
     
     def get_description(self):
-        self.description = input('Write your daily diary entry:\n')
+        self.description = input('Please enter diary entry:\n')
 
-    def save(self):
-        filename = f'{self.created}.diary'
+    def serialize(self):
+        return {
+            "description": self.description,
+            "created": self.created
+        }
 
-        with open(filename, 'w') as f:
-            f.write(self.description)
+
+def main():
+    diary = Diary()
+    diary.load()
+
+    entry = Entry()
+    entry.get_description()
+    diary.add_entry(entry)
+    diary.save()
 
 
+if __name__ == "__main__":
+    main()
